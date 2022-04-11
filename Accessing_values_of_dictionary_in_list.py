aea04d7fdee469151e03d83377fe040b9930a7fe
Saipreethi2003 <100211700+Saipreethi2003@users.noreply.github.com>
2022-04-11 19:33:19 +0530

    Create Accessing_values_of_dictionary_in_list.py

---
+++ Accessing_values_of_dictionary_in_list.py
@@ -0,0 +1,9 @@
+Food = {
+ "Fastfood" : ["Manchurian", "pani puri", "pau baaji"],
+ "Sweets" : ["Khaju katli", "Milk maisoor", "Gulab jamun"],
+ "Deserts" : ["Tres leaches", "Ice cream", "Pastries"]
+}
+print("Food["Sweets"])
+print("Food["Sweets"][0])
+print("Food["Fastfood"][1])
+print("Food["Deserts"][2])
